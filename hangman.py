82dfca552b31eeb83d4764245b03e5d26ff64afd
yana5 <yana5nizkaya@ya.ru>
2024-01-13 18:39:34 +0300

    готовая змейка

---
+++ hangman.py
@@ -0,0 +1,106 @@
+import random
+import os
+
+
+def field(a):
+    f0 = r'''
+       +----+
+       |    |
+       o    |
+      /|\   |
+      / \   |
+    _______/|\_
+    '''
+    f1 = r'''
+       +----+
+       |    |
+       o    |
+      /|\   |
+            |
+    _______/|\_
+    '''
+    f2 = r'''
+       +----+
+       |    |
+       o    |
+            |
+            |
+    _______/|\_
+    '''
+    f3 = r'''
+       +----+
+       |    |
+            |
+            |
+            |
+    _______/|\_
+    '''
+    f4 = r'''
+       +----+
+            |
+            |
+            |
+            |
+    _______/|\_
+    '''
+    f5 = r'''
+            +
+            |
+            |
+            |
+            |
+    _______/|\_
+    '''
+    f6 = r'''
+
+
+
+
+
+    _______/|\_
+    '''
+    f7 = " "
+    arr = [f0, f1, f2, f3, f4, f5, f6, f7]
+    return arr[a]
+
+
+def create_secret():
+    dict = [i for i in open("dictionary")]
+    i = random.randint(0, len(dict))
+    return dict[i]
+
+
+SECRET = create_secret()
+WORD = ['_' for i in range(len(SECRET) - 1)]
+already_named = set()
+TRIES = 7
+FIELD = field(TRIES)
+print(*WORD)
+while TRIES > 0:
+    # make a move!
+    letter = input('Enter your guess: ')
+    if letter not in SECRET:
+        TRIES -= 1
+        already_named.add(letter)
+        FIELD = field(TRIES)  # если не угадали, то надо обновить поле
+
+    else:
+        unknown_let = 0
+        for i in range(len(SECRET) - 1):
+            if SECRET[i] == letter:
+                WORD[i] = letter
+            if WORD[i] == '_':
+                unknown_let += 1
+
+        if unknown_let == 0:
+            print(*WORD)
+            print("YOU WIN!!")
+            break
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("не подошли: ", *already_named)
+    print(*WORD)
+    print(FIELD)
+if TRIES == 0:
+    print(SECRET)
+    print("YOU'VE LOST\n" + "DON'T WORRY, TRY AGAIN!")
---
+++ snake.py
@@ -0,0 +1,107 @@
+from pynput import keyboard
+from random import randint
+import os
+import threading
+import time
+mutex = threading.Lock()
+def spawnApple():
+    while True:
+        tryApple = random_position()
+        if tryApple not in snake:
+            return tryApple
+def printField():
+    os.system('cls')
+    for i in range(WIDTH):
+        for j in range(HEIGHT):
+            if (i, j) in snake:
+                print(0, end=' ')
+            elif (i,j) == apple:
+                print('A', end=' ')
+            else:
+                print('_', end=' ')
+        print()
+
+def scrawl():
+    #threading.Timer(1.0, scrawl).start()
+    global stop
+    deleteLastCoor = True
+    while stop == False:
+        time.sleep(0.2)
+
+        x = snake[0][0]+direction[0]
+        y = snake[0][1] + direction[1]
+        snake.insert(0,(x,y))
+
+        if deleteLastCoor == True:
+            snake.pop()
+        global apple
+        if snake[0] == apple:
+            deleteLastCoor = False
+            apple = spawnApple()
+        else:
+            deleteLastCoor = True
+        printField()
+        if keepMoving()==False:
+            print("Game over")
+            stop = True
+            return 0
+            #threading.Timer(1.0, scrawl).join()
+
+
+
+def keepMoving():
+
+    if (len(snake)>1 and snake[0] in snake[1:]) or snake[0][0]==-1 or snake[0][1]==-1 or snake[0][0]==HEIGHT or snake[0][1]==WIDTH:
+        return False
+    return True
+
+
+def random_position():
+    return randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
+
+
+def process_press(key):
+    # обработчик нажатия на клавиши (можно сделать и поаккуратнее)
+    global direction
+    match key:
+        case keyboard.Key.left:
+            direction = (0, -1)
+        case keyboard.Key.up:
+            direction = (-1, 0)
+        case keyboard.Key.right:
+            direction = (0, 1)
+        case keyboard.Key.down:
+            direction = (1, 0)
+    if stop == True:
+        listener.stop()
+#def on_release(key):
+#    if STOP==True:
+        # Stop listener
+#        return False
+
+WIDTH, HEIGHT = 10, 10
+stop = False
+# можно приделать конфиг-файл с параметрами
+direction = (1, 0)
+
+snake = [random_position()]
+apple = random_position()
+printField()
+
+
+'''def press():
+    while True:  # making a loop
+        try:  # used try so that if user pressed other than the given key error will not be shown
+            if keyboard.is_pressed('q'):  # if key 'q' is pressed
+                print('You Pressed A Key!')
+                break  # finishing the loop
+        except:
+            break
+t = threading.Thread(target=press())'''
+run = threading.Thread(target=scrawl)
+run.start()
+with keyboard.Listener(on_press=process_press) as listener:
+    listener.join()
+
+
+run.join()
